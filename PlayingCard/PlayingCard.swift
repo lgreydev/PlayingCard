ccef5f86fb0b37a69abae84845073be296d751b3
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-01 12:31:20 +0300

    Update model 'PlayingCard'

--- PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+
+struct PlayingCard {
+    
+    var suit: Suit
+    var Rank: Rank
+    
+    static var all: [Rank] {
+        var allRanks: [Rank] = [.ace]
+        for pips in 2...10 {
+            allRanks.append(.numeric(pips))
+        }
+        allRanks += [.face("J"), .face("Q"), .face("K")]
+        return allRanks
+    }
+    
+    enum Suit: String {
+        case spades = "♠️"
+        case hearts = "♥️"
+        case clubs = "♣️"
+        case diamonds = "♦️"
+        static var all = [Suit.spades, .hearts, .clubs, .diamonds]
+    }
+
+    enum Rank {
+        case ace
+        case face(String)
+        case numeric(Int)
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default:
+                return 0
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
