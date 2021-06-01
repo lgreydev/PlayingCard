09ef61553e823265e9b6ddc82914af569f30c26f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-01 14:42:48 +0300

    Update 'PlayingCardDeck' add 'init()'

--- PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard.swift
@@ -8,32 +8,28 @@
 import Foundation
 
 
-struct PlayingCard {
+struct PlayingCard: CustomStringConvertible {
+    
+    var description: String { return "\(rank) \(suit)"}
     
     var suit: Suit
-    var Rank: Rank
+    var rank: Rank
     
-    static var all: [Rank] {
-        var allRanks: [Rank] = [.ace]
-        for pips in 2...10 {
-            allRanks.append(.numeric(pips))
-        }
-        allRanks += [.face("J"), .face("Q"), .face("K")]
-        return allRanks
-    }
-    
-    enum Suit: String {
+
+    enum Suit: String, CustomStringConvertible {
         case spades = "♠️"
         case hearts = "♥️"
         case clubs = "♣️"
         case diamonds = "♦️"
+        var description: String { return self.rawValue }
         static var all = [Suit.spades, .hearts, .clubs, .diamonds]
     }
 
-    enum Rank {
+    enum Rank: CustomStringConvertible {
         case ace
         case face(String)
         case numeric(Int)
+        var description: String { return "\(self.order)" }
         var order: Int {
             switch self {
             case .ace: return 1
@@ -45,18 +41,14 @@ struct PlayingCard {
                 return 0
             }
         }
+        static var all: [Rank] {
+            var allRanks: [Rank] = [.ace]
+            for pips in 2...10 {
+                allRanks.append(.numeric(pips))
+            }
+            allRanks += [.face("J"), .face("Q"), .face("K")]
+            return allRanks
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
--- PlayingCard/PlayingCardDeck.swift
+++ PlayingCard/PlayingCardDeck.swift
@@ -11,6 +11,16 @@ import Foundation
 struct PlayingCardDeck {
     private(set) var cards = [PlayingCard]()
     
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+            
+        }
+    }
+    
     mutating func draw() -> PlayingCard? {
         if cards.count > 0 {
             return cards.remove(at: cards.count.random)
