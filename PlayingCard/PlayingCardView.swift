e46ea3b5880fde416f72ee2d15c83c0e7d787283
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-20 10:11:48 +0200

    feat: draw playing card

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,43 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Sergey Lukaschuk on 20.11.2021.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+    
+    
+    override func draw(_ rect: CGRect) {
+        /*
+        if let context = UIGraphicsGetCurrentContext() {
+            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
+                           radius: 100,
+                           startAngle: 0,
+                           endAngle: 2*CGFloat.pi,
+                           clockwise: true)
+            
+            context.setLineWidth(5.0)
+            UIColor.green.setFill()
+            UIColor.red.setStroke()
+            context.strokePath()
+            context.fillPath()
+        }
+        */
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
+                    radius: 100,
+                    startAngle: 0,
+                    endAngle: 2*CGFloat.pi,
+                    clockwise: true)
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.lineWidth = 5.0
+        path.stroke()
+        path.fill()
+    }
+    
+    
+}
