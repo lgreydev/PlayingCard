e19e782d653ec00b35f8370ae17cf2707da61e38
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-23 23:32:30 +0200

    feat: add Accessibility size of larger text

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -26,36 +26,11 @@ class PlayingCardView: UIView {
         roundedRect.addClip()
         UIColor.white.setFill()
         roundedRect.fill()
-        
-        /*
-        if let context = UIGraphicsGetCurrentContext() {
-            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
-                           radius: 100,
-                           startAngle: 0,
-                           endAngle: 2*CGFloat.pi,
-                           clockwise: true)
-            
-            context.setLineWidth(5.0)
-            UIColor.green.setFill()
-            UIColor.red.setStroke()
-            context.strokePath()
-            context.fillPath()
-        }
-        */
-        
-        /*
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
-                    radius: 100,
-                    startAngle: 0,
-                    endAngle: 2*CGFloat.pi,
-                    clockwise: true)
-        UIColor.green.setFill()
-        UIColor.red.setStroke()
-        path.lineWidth = 5.0
-        path.stroke()
-        path.fill()
-         */
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setNeedsDisplay()
+        setNeedsLayout()
     }
     
     override func layoutSubviews() {
@@ -86,7 +61,7 @@ class PlayingCardView: UIView {
         return label
     }
     
-    func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
         font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
         
